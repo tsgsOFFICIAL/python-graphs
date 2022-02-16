bec579ee16c915712fcd2da16583492c60935726
Marcus Mortensen Jensen <marc7075@zbc.dk>
2022-02-16 09:10:34 +0100

    Added comments

--- matplot-demo.py
+++ matplot-demo.py
@@ -2,6 +2,7 @@ import json
 from urllib.request import urlopen
 import matplotlib.pyplot as plt
 
+# This is just an "enum" for colors in the console
 class color:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -13,51 +14,59 @@ class color:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-# Variables
+# Initialize all of the empty list's
 dkk_results = []
 eur_results = []
 hour_results = []
 
-print(f"\n{color.BOLD}{color.HEADER}Getting data from API: \n")
+# Print to console, using colors
+print(f"{color.BOLD}{color.HEADER}Getting data from API: \n")
 print("\n"*9)
 print(color.ENDC)
 
+# API Url
 url = "https://api.energidataservice.dk/datastore_search_sql?sql=SELECT%20%22HourDK%22,%20%22PriceArea%22,%20%22SpotPriceDKK%22,%20%22SpotPriceEUR%22%20FROM%20%22elspotprices%22%20WHERE%20%22PriceArea%22%20=%20%27DK2%27%20ORDER%20BY%20%22HourDK%22%20DESC%20LIMIT%2048"
 
+# Get the response from opening the url, and automatically close it again
 response = urlopen(url)
 
+# Deserialize from json text, to a python dictionary
 json_data = json.loads(response.read())
 
+# Parse through the json "object", get the "result"
 data = json_data["result"]
 
+# And from within the "result", get the records, this is where the data is.
 records = data["records"]
 for record in records:
-    dkk_results.append(record["SpotPriceDKK"])
-    eur_results.append(record["SpotPriceEUR"])
-    hour_results.append(record["HourDK"].split("T")[1].split(":")[0])
+    dkk_results.append(record["SpotPriceDKK"]) # Append / Add to list
+    eur_results.append(record["SpotPriceEUR"]) # Append / Add to list
+    hour_results.append(record["HourDK"].split("T")[1].split(":")[0]) # Append / Add to list | 2022-02-15T00:00:00
 
-dkk_results1 = dkk_results[:len(dkk_results)//2]
-dkk_results2 = dkk_results[len(dkk_results)//2:]
+dkk_results1 = dkk_results[:len(dkk_results)//2] # Get the first half of the list
+dkk_results2 = dkk_results[len(dkk_results)//2:] # Get the second half of the list
 
-eur_results1 = eur_results[:len(eur_results)//2]
-eur_results2 = eur_results[len(eur_results)//2:]
+eur_results1 = eur_results[:len(eur_results)//2] # Get the first half of the list
+eur_results2 = eur_results[len(eur_results)//2:] # Get the second half of the list
 
-hour_results = hour_results[:len(hour_results)//2]
+hour_results = hour_results[:len(hour_results)//2] # Get the first half of the list
 
-dkk_results1 = dkk_results1[::-1]
-eur_results1 = eur_results1[::-1]
+dkk_results1 = dkk_results1[::-1] # Reverse the list
+eur_results1 = eur_results1[::-1] # Reverse the list
 
-dkk_results2 = dkk_results2[::-1]
-eur_results2 = eur_results2[::-1]
+dkk_results2 = dkk_results2[::-1] # Reverse the list
+eur_results2 = eur_results2[::-1] # Reverse the list
 
-hour_results = hour_results[::-1]
+hour_results = hour_results[::-1] # Reverse the list
 
 
+# Print to console
 print(f"\n{color.BOLD}{color.HEADER}Generating curves: \n")
 print("\n"*9)
 print(color.ENDC)
 
 
+# Create the plot/graph
 plt.title("Engery prices for today & tomorow")
 plt.ylabel("Price (DKK & EUR)")
 plt.xlabel("Time (HOUR)")
